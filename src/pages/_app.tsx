ac74a0645bad50d91aa83140715a43ec013e2b99
sondq <sondq@vng.com.vn>
2024-12-17 09:52:07 +0700

    update loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import type { AppProps } from "next/app";
 import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }: AppProps) {
-  <NextNProgress color="#29D" startPosition={0.3} stopDelayMs={500} height={6} showOnShallow={false}/>
-  return <Component {...pageProps} />;
+  
+  return (
+    <>
+      <NextNProgress color="#29D" startPosition={0.3} stopDelayMs={500} height={6} showOnShallow={false}/>
+      <Component {...pageProps} />
+    </>
+  );
 }
