ecbc42563ef53b0ec6079b13d3de7444861b0ae5
sondq <sondq@gmai.com>
2025-03-10 22:15:06 +0700

    update

--- src/pages/editor/edit/[id].tsx
+++ src/pages/editor/edit/[id].tsx
@@ -134,7 +134,6 @@ const Editor =  () =>{
       <>
         {/* <Button><Link href="/dashboard">Home</Link></Button> */}
         <Tabs></Tabs>
-        <Button  onClick={()=>{          saveMdx(parseInt(slug.current));        }} >Save</Button>        
         { saveStatus.length > 0 &&
         <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> {saveStatus}</button>
         }
