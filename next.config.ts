775bd3979ad1bd4403c32a93ebef32675ad809b2
DANG QUOC SON <sondq@vng.com.vn>
2025-12-17 15:47:42 +0700

    build: Configure Next.js to transpile specific packages and enable top-level await in webpack.

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
       pathname: '/be/s3/**'
     }],
   },
+  transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    return config;
+  },
   //  compress
   compress: true,
   allowedDevOrigins: ['103.82.133.178'],
